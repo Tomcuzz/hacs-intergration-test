25d348902bab9f66e6699c87e62a809cd3bae685
Thomas Cousin <thomas.cousin@me.com>
2025-10-05 00:24:44 +0100

    Update init

--- custom_components/hacs-intergration-test/__init__.py
+++ custom_components/hacs-intergration-test/__init__.py
@@ -1,4 +1,8 @@
-{
-  "domain": "hacs_intergration_test",
-  "name": "Hello, state!"
-}
+DOMAIN = "hacs_intergration_test"
+
+
+async def async_setup(hass, config):
+    # hass.states.async_set("hello_state.world", "Paulus")
+
+    # Return boolean to indicate that initialization was successful.
+    return True
